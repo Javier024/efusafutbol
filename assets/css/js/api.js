1b00478e7d01b2efcb448fff7698916f0f6bb098
javier guzman <jguzmanmacea93@correo.unicordoba.edu.co>
2026-02-12 20:21:05 -0500

    Agrega sistema EFUSA completo

--- assets/css/js/api.js
+++ assets/css/js/api.js
@@ -1,8 +1,8 @@
 // js/api.js
 class API {
   constructor() {
-    // URL del script de Google Apps (deberás actualizarla con tu script desplegado)
-    this.baseUrl = "https://script.google.com/macros/s/AKfycbxi8pnqdpjBiMzfwv3kLFGritmgC9tg5oVMxKFugunQYtAwBZVnXnkPvEycOZYMEpAgnA/exec";
+    // URL del script de Google Apps (actualizada con tu enlace)
+    this.baseUrl = "https://script.google.com/macros/s/AKfycbxzFZkz7dWuUQlY4Df4ky8XIaPjpK3fFVlR96aTSRe8BbqK3X60LD6QWTtGKP4XFu-BlA/exec";
   }
 
   async request(action, params = {}) {
@@ -10,17 +10,27 @@ class API {
       const url = new URL(this.baseUrl);
       url.searchParams.append('action', action);
       
+      // Los datos complejos (objetos) se deben enviar como string JSON
+      const queryParams = new URLSearchParams();
       Object.keys(params).forEach(key => {
-        url.searchParams.append(key, params[key]);
+        if (typeof params[key] === 'object') {
+          queryParams.append(key, JSON.stringify(params[key]));
+        } else {
+          queryParams.append(key, params[key]);
+        }
       });
+      
+      // Unimos los parámetros de la acción y los datos
+      const finalUrl = `${url.toString()}&${queryParams.toString()}`;
 
-      const response = await fetch(url.toString());
+      const response = await fetch(finalUrl);
       if (!response.ok) throw new Error('Error en la solicitud');
       
       return await response.json();
     } catch (error) {
       console.error('Error en API:', error);
-      throw error;
+      // Devolvemos un error consistente para que el frontend lo maneje
+      return { status: 'error', message: error.message };
     }
   }
 
@@ -34,7 +44,8 @@ class API {
   }
 
   async saveJugador(jugador) {
-    return this.request('saveJugador', jugador);
+    // El método saveJugador ahora enviará el objeto completo
+    return this.request('saveJugador', { data: jugador });
   }
 
   async deleteJugador(id) {
@@ -51,7 +62,7 @@ class API {
   }
 
   async savePago(pago) {
-    return this.request('savePago', pago);
+    return this.request('savePago', { data: pago });
   }
 
   async deletePago(id) {
@@ -64,5 +75,5 @@ class API {
   }
 }
 
-// Instancia global de la API
+// Instancia global de la API para que sea accesible desde cualquier parte de la aplicación
 window.api = new API();
